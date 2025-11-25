e541498302f1ff2e61e2f9b68a096e454118bc41
Apetrei-Alexandru <apetrei.alex2004@gmail.com>
2025-11-25 16:03:12 +0200

    Rezolvare Ex3

--- Program.cs
+++ Program.cs
@@ -21,6 +21,13 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "start",
+    pattern: "start",
+    defaults: new { controller = "Home", action = "Index" });
+
+
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
