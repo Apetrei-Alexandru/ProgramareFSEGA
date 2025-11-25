f848d4e81c047a453818a20b1d1f144c8c4ae2d8
Apetrei-Alexandru <apetrei.alex2004@gmail.com>
2025-11-25 16:08:27 +0200

    Rezolvare Ex4

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apetrei_Alexandru_Lab1.Controllers
+{
+    public class DemoController : Controller
+    {
+        // Rulare la /hello
+        [Route("hello")]
+        public string Hello() => "Salut!";
+
+        // Rulare la /hello/Nume
+        [HttpGet("hello/{name}")]
+        public string HelloByName(string name) => $"Salut, {name}!";
+
+        // Rulare la /calc/a/b (numere întregi)
+        [HttpGet("calc/{a:int}/{b:int}")]
+        public string Calculate(int a, int b) => $"Suma este: {a + b}";
+    }
+}
